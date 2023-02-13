4777cd9aab3583545dd18683c1f2e436d1c40994
Anna Marushchak <maruschakanya86@gmail.com>
2023-02-13 14:41:45 +0100

    Edit file

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-import requests
+from flask import Flask, request, render_template, redirect, url_for
 
-response = requests.get("https://todos.json")
-data = response.json()
-
-from flask import Flask, request
-
+from forms import TodoForm
+from models import todos
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "ilikemykey"
+
+@app.route("/todos/", methods=["GET", "POST"])
+def todos_list():
+  form = TodoForm()
+  error = ""
+  if request.method == "POST":
+    if form.validate_on_submit():
+      todos.create(form.data)
+      todos.save_all()
+    return redirect(url_for("todos_list"))
+
+  return render_template("todos.html", form=form, todos=todos.all(), error=error)
 
 
-@app.route('/todos/', methods=['GET'])
-def get_list():
-   if request.method == 'GET':
-       data = response.json()
-       return data
+@app.route("/todos/<int:todo_id>/", methods=["GET", "POST"])
+def todo_details(todo_id):
+  todo = todos.get(todo_id - 1)
+  form = TodoForm(data=todo)
+
+  if request.method == "POST":
+    if form.validate_on_submit():
+      todos.update(todo_id - 1, form.data)
+    return redirect(url_for("todos_list"))
+  return render_template("todo.html", form=form, todo_id=todo_id)
 
 
 if __name__ == "__main__":
-    app.run()
+  app.run(debug=True)
