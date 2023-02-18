a78b29dc182e90bbf46950dae67838e6d15b054e
Anna Marushchak <maruschakanya86@gmail.com>
2023-02-18 09:55:55 +0100

    Edit file

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from flask import Flask, request, render_template, redirect, url_for
-
+from collections import namedtuple
+from forms import EmailPasswordForm
 from forms import TodoForm
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField
 from models import todos
 
+
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ilikemykey"
+
+
+User = namedtuple("User", field_names=["email", "password"])
+user = User(email="john@black.com", password="black")
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+  form = EmailPasswordForm()
+  error = ""
+  if request.method == "POST":
+    if form.validate_on_submit():
+      if form.email.data == user.email and form.password.data == user.password:
+        return "You are logged id"
+      else:
+        return "Wrong credentials!!"
+    else:
+      error = form.errors
+  return render_template("login.html", form=form, error=error)
+
 
 @app.route("/todos/", methods=["GET", "POST"])
 def todos_list():
@@ -35,3 +57,11 @@ def todo_details(todo_id):
 
 if __name__ == "__main__":
   app.run(debug=True)
+
+
+
+
+
+
+
+  
---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,TextAreaField, BooleanField
+from wtforms.validators import DataRequired,Email
+
+class LoginForm(FlaskForm):
+  username = StringField('username', validators=[DataRequired()])
+  password = PasswordField('password', validators=[DataRequired()])
+
+class EmailPasswordForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
